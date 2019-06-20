c85c55960304d1987d3668452eb3adccb140e567
Roman Orlovskyi <Roman_Orlovskyi@epam.com>
2019-06-20 16:55:30 +0300

    Minor refactor #2 for aws-network module

--- modules/aws-network/main.tf
+++ modules/aws-network/main.tf
@@ -1,30 +1,3 @@
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC."
-}
-
-variable "external_subnets_cidr" {
-  description = "List of external subnets"
-  type        = "list"
-}
-
-variable "internal_subnets_cidr" {
-  description = "List of internal subnets"
-  type        = "list"
-}
-
-variable "environment" {
-  description = "Environment tag, e.g prod"
-}
-
-variable "availability_zones" {
-  description = "List of availability zones"
-  type        = "list"
-}
-
-variable "name" {
-  description = "Name tag, e.g stack"  
-}
-
 /**
  * VPC
  */
@@ -148,43 +121,4 @@ resource "aws_route_table_association" "external" {
   count          = "${length(var.external_subnets_cidr)}"
   subnet_id      = "${element(aws_subnet.external.*.id, count.index)}"
   route_table_id = "${aws_route_table.external.id}"
-}
-
-/**
- * Outputs
- */
-
-// The VPC ID
-output "id" {
-  value = "${aws_vpc.main.id}"
-}
-
-// The VPC CIDR
-output "cidr_block" {
-  value = "${aws_vpc.main.cidr_block}"
-}
-
-// A comma-separated list of subnet IDs.
-output "external_subnets" {
-  value = ["${aws_subnet.external.*.id}"]
-}
-
-// A list of subnet IDs.
-output "internal_subnets" {
-  value = ["${aws_subnet.internal.*.id}"]
-}
-
-// The default VPC security group ID.
-output "security_group" {
-  value = "${aws_vpc.main.default_security_group_id}"
-}
-
-// The list of availability zones of the VPC.
-output "availability_zones" {
-  value = ["${aws_subnet.external.*.availability_zone}"]
-}
-
-// The list of EIPs associated with the internal subnets.
-output "internal_nat_ips" {
-  value = ["${aws_eip.nat.*.public_ip}"]
 }
