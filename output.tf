0dcbf87065aac71b7c5b17a9d47a5a935998e19a
Roman Orlovskyi <Roman_Orlovskyi@epam.com>
2019-06-25 17:11:54 +0300

    Back to the correct version after adding modifications for terraform-docs

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "service_url" {
   description = "Application Load balancer URL"
-  value = "https://${module.ecs_cluster.alb_url}"
+  value       = "https://${module.ecs_cluster.alb_url}"
 }
